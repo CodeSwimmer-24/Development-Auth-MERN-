67b325ba07287faf5e2c5d0822b3ff3d177998b0
Fahad <fahadmahmood1200@gmail.com>
2021-06-22 23:02:20 +0530

    Third commit

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -1,28 +1,32 @@
 const User = require("../Model/user");
 
-exports.signup = (req, res) => {
-  // console.log("Req body on signup", req.body);
-  const { name, email, password } = req.body;
+// exports.signup = (req, res) => {
+//   // console.log("Req body on signup", req.body);
+//   const { name, email, password } = req.body;
 
-  User.findOne({ email }).exec((err, user) => {
-    if (user) {
-      return res.status(400).json({
-        error: "Email is taken",
-      });
-    }
-  });
+//   User.findOne({ email }).exec((err, user) => {
+//     if (user) {
+//       return res.status(400).json({
+//         error: "Email is taken",
+//       });
+//     }
+//   });
 
-  let newUser = new User({ name, email, password });
+//   let newUser = new User({ name, email, password });
 
-  newUser.save((err, sucess) => {
-    if (err) {
-      console.log("SIGNUP ERROR", err);
-      return res.status(400).json({
-        error: err,
-      });
-    }
-    res.json({
-      message: "Signup sucess! Please login",
-    });
-  });
-};
+//   newUser.save((err, success) => {
+//     if (err) {
+//       console.log("SIGNUP ERROR", err);
+//       return res.status(400).json({
+//         error: err,
+//       });
+//     }
+//     res.json({
+//       message: "Signup sucess! Please login",
+//     });
+//   });
+// };
+
+exports.signup = (req,res) => {
+  
+}
